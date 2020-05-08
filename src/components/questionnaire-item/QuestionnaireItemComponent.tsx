24cd2e6384ac0856a8a58c480e2b1d143531c5cb
bryant <bryant@alphora.com>
2020-05-08 09:45:30 -0600

    choice types to handle different answers using code systems/valuesets

--- src/components/questionnaire-item/QuestionnaireItemComponent.tsx
+++ src/components/questionnaire-item/QuestionnaireItemComponent.tsx
@@ -16,11 +16,7 @@ function QuestionnaireItemComponent(props: { QuestionnaireItem: QuestionnaireIte
             </div>
           : props.QuestionnaireItem.type === "choice" ?
             <div>
-              <select onChange={(event) => props.onChange(props.QuestionnaireItem, [{ valueString: event.target.value }])}>
-                <option>One</option>
-                <option>Two</option>
-                <option>Three</option>
-              </select>
+                {populateChoice(props)}
             </div>
           : props.QuestionnaireItem.type === "quantity" ?
             <div>
@@ -43,6 +39,33 @@ function QuestionnaireItemComponent(props: { QuestionnaireItem: QuestionnaireIte
       </div>
     </div>
   );
+}
+
+function populateChoice(props: { QuestionnaireItem: QuestionnaireItem, onChange: (item: QuestionnaireItem, answer?: QuestionnaireResponseItemAnswer[]) => void }){
+    if(props.QuestionnaireItem.answerValueSet === 'http://rti.com/fhir/rti/ValueSet/pain-assessments-intensity'){
+        return(
+            <select onChange={(event) => props.onChange(props.QuestionnaireItem, [{ valueString: event.target.value }])}>
+                <option></option>
+                <option>no pain</option>
+                <option>mild pain</option>
+                <option>moderate pain</option>
+                <option>severe pain</option>
+                <option>very severe pain</option>
+            </select>
+        );
+    }
+    if(props.QuestionnaireItem.answerValueSet === 'http://rti.com/fhir/rti/ValueSet/pain-assessments-interference'){
+        return(
+            <select onChange={(event) => props.onChange(props.QuestionnaireItem, [{ valueString: event.target.value }])}>
+                <option></option>
+                <option>Not at all</option>
+                <option>A little bit</option>
+                <option>Somewhat</option>
+                <option>Quite a bit</option>
+                <option>Very much</option>
+            </select>
+        );
+    }
 }
 
 export default QuestionnaireItemComponent;
