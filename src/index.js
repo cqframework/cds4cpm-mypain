32d845028689c65d334421c31ca2b35f904765a8
Jason Evans <jason@databaseconsultinggroup.com>
2020-06-05 09:14:01 -0600

    Add gh-pages support and change s3 deploy path

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App basename={process.env.PUBLIC_URL} />
   </React.StrictMode>,
   document.getElementById('root')
 );
