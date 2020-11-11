57eceed30c6cdd04cf7b515aa60ee5b3dc8ab2e5
Patrick <hardig_pm@live.com>
2020-11-11 12:54:49 -0500

    removed unused code.

--- src/components/review-page/ReviewPageComponent.tsx
+++ src/components/review-page/ReviewPageComponent.tsx
@@ -90,56 +90,5 @@ export default class ReviewPageComponent extends React.Component<any, any> {
             }
         })
 
-        //         return (
-        //             <div>
-        //                 {
-        //                     <Table responsive bordered striped>
-        //                         <thead>
-        //                             <tr>
-        //                                 <th>Question</th>
-        //                                 <th>Answer</th>
-        //                             </tr>
-        //                         </thead>
-        //                         <tbody>
-        //                             {this.props.item.map((item: any) => {
-        //                                 if (item.answer.valueCoding) {
-        //                                     if (item.answer.valueCoding.display) {
-        //                                         return (<tr key={JSON.stringify(item)}>
-        //                                             <td> {parser(item.text)} </td>
-        //                                             <td> {JSON.stringify(item.answer[0].valueCoding.display)}</td>
-        //                                         </tr>)
-
-
-        //                                     }
-        //                                     else {
-        //                                         return <tr>
-        //                                             <td>Other:</td>
-        //                                             <td>{JSON.stringify(item)}</td>
-        //                                         </tr>
-        //                                     }
-        //                                 }
-        //                                 else if (item.answer.valueString) {
-        //                                     return (<tr key={JSON.stringify(item)}>
-        //                                         <td> {parser(item.text)} </td>
-        //                                         <td> {item.answer.valueString}</td>
-        //                                     </tr>)
-        //                                 } else {
-        //                                     console.log('not item: ', item)
-        //                                     return <div></div>;
-        //                                 }
-        //                             }
-        //                             )
-        //                             }
-        //                         </tbody>
-        //                     </Table>
-
-        //                 }
-        //             </div>
-        //         )
-        //     })
-
-        // } else {
-        //     return <div></div>
-        // }
     }
 }
