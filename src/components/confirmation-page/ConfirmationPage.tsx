2ed05dae27b064ecd37a5fee9844beb7e57828d5
Bryant Austin <bryantaustin13@yahoo.com>
2022-03-29 14:37:55 -0600

    update fontawsome versions for errors

--- src/components/confirmation-page/ConfirmationPage.tsx
+++ src/components/confirmation-page/ConfirmationPage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import './ConfirmationPage.css';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
 
 
 
@@ -9,7 +10,7 @@ export const ConfirmationPage = () => {
 
     return <div className="confirmation-page">
         <div className="confirmation-container">
-            <h1> <FontAwesomeIcon icon={faCheckCircle} /></h1>
+            <h1> <FontAwesomeIcon icon={faCheckCircle as IconProp} /></h1>
             <h2>Congratulations!</h2>
             <p>Your responses have been successfully recorded. You may now close your browser window.</p>
         </div>
